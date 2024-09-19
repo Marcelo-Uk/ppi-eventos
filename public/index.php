5c075d0967033a5ed4798f5ef932642487289e10
marcelo-uk <mribeirocorp@gmail.com>
2024-09-19 16:45:37 -0300

    --login ok --190924 --1645h

--- public/index.php
+++ public/index.php
@@ -4,18 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="/eventos/public/css/style.css"> <!-- Linkando o seu arquivo CSS principal -->
 </head>
 <body>
-    <h2>Login</h2>
-    <form method="POST" action="login.php">
-        <label>Email:</label>
-        <input type="email" name="email" required>
-        <br>
-        <label>Senha:</label>
-        <input type="password" name="password" required>
-        <br>
-        <input type="submit" value="Entrar">
-    </form>
+    <div class="login-container">
+        <div class="login-box">
+            <h2>Login</h2>
+            <form method="POST" action="login.php">
+                <label for="email">Email</label>
+                <input type="email" name="email" placeholder="Digite seu email" required>
+
+                <label for="password">Senha</label>
+                <input type="password" name="password" placeholder="Digite sua senha" required>
+
+                <button type="submit">Entrar</button>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
-
